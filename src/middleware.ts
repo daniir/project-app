caf9eb0b207db600fff9e762db19183b4d969efe
daniir <danrodas03@gmail.com>
2024-08-03 19:40:11 -0500

    Project-app

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,29 @@
 import NextAuth from 'next-auth';
 import authConfig from './auth.config';
+import { NextResponse } from 'next/server';
 
-export const { auth: middleware } = NextAuth(authConfig);
+const { auth: middleware } = NextAuth(authConfig);
+
+export default middleware((req) => {
+  if (req.nextUrl.pathname === '/') {
+    if (!req.auth?.user)
+      return NextResponse.redirect(new URL('/login', req.nextUrl));
+
+    return NextResponse.next();
+  }
+
+  if (
+    req.nextUrl.pathname === '/login' ||
+    req.nextUrl.pathname === '/register'
+  ) {
+    if (!req.auth?.user) return NextResponse.next();
+
+    return NextResponse.redirect(new URL('/', req.nextUrl));
+  }
+
+  return NextResponse.next();
+});
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
